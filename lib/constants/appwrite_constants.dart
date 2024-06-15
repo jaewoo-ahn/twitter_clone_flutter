195a88527c2af7b7544bbaa9f024774b76ab6643
Jaewoo ahn <ahnjaewoo3042@gmail.com>
2024-06-15 20:43:13 +0900

    assets

---
+++ lib/constants/appwrite_constants.dart
@@ -0,0 +1,5 @@
+class AppwriteConstants {
+  static const String databaseId = "666d7cd60004d3dfcb3e";
+  static const String projectId = "666d7a59002ef9900e62";
+  static const String endPoint = "http://localhost:80/v1";
+}
